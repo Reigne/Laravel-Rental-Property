c2904efddb1448028367a286f94432eb700e4d27
Reigne <monteronaelija@yahoo.com>
2023-04-13 18:45:17 +0800

    fixed image for admin

--- resources/views/admin/verification.blade.php
+++ resources/views/admin/verification.blade.php
@@ -17,7 +17,7 @@
                 <div class="row justify-content-center align-items-center">
                     <div class="col-sm-auto col-4">
                         <div class="avatar avatar-xl position-relative">
-                            <img src="{{ Auth::user()->admins->imagePath }}" alt="bruce"
+                            <img src="{{ asset(Auth::user()->admins->imagePath) }}" alt="bruce"
                                 class="w-100 border-radius-lg shadow-sm">
                         </div>
                     </div>
